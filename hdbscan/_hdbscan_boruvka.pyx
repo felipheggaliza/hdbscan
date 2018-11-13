d323a75b2007fac41a1e1089273455931f9d45f1
Feliphe Gonçalves Galiza <feliphe.ggaliza@gmail.com>
2018-11-13 12:31:10 -0200

    Improved multiprocessing
    Added code to support more than 4 jobs.

--- hdbscan/_hdbscan_boruvka.pyx
+++ hdbscan/_hdbscan_boruvka.pyx
@@ -315,7 +315,7 @@ cdef class KDTreeBoruvkaAlgorithm (object):
     cdef np.ndarray candidate_distance_arr
 
     def __init__(self, tree, min_samples=5, metric='euclidean', leaf_size=20,
-                 alpha=1.0, approx_min_span_tree=False, n_jobs=4, **kwargs):
+                 alpha=1.0, approx_min_span_tree=False, n_jobs=4, n_chunks=4,**kwargs):
 
         self.core_dist_tree = tree
         self.tree = KDTree(tree.data, metric=metric, leaf_size=leaf_size,
@@ -327,6 +327,7 @@ cdef class KDTreeBoruvkaAlgorithm (object):
         self.alpha = alpha
         self.approx_min_span_tree = approx_min_span_tree
         self.n_jobs = n_jobs
+        self.n_chunks = n_chunks
 
         self.num_points = self.tree.data.shape[0]
         self.num_features = self.tree.data.shape[1]
@@ -395,18 +396,20 @@ cdef class KDTreeBoruvkaAlgorithm (object):
         cdef np.ndarray[np.intp_t, ndim=2] knn_indices
 
         # A shortcut: if we have a lot of points then we can split the points
-        # into four piles and query them in parallel. On multicore systems
-        # (most systems) this amounts to a 2x-3x wall clock improvement.
-        if self.tree.data.shape[0] > 16384 and self.n_jobs > 1:
-            datasets = [
-                np.asarray(self.tree.data[0:self.num_points//4]),
-                np.asarray(self.tree.data[self.num_points//4:
-                                          self.num_points//2]),
-                np.asarray(self.tree.data[self.num_points//2:
-                                          3*(self.num_points//4)]),
-                np.asarray(self.tree.data[3*(self.num_points//4):
-                                          self.num_points])
-                        ]
+        # into  n_chunks and query them in parallel. On multicore systems
+        # (most systems) this amounts to a performance improvement.
+        if self.tree.data.shape[0] > 16384 and self.n_jobs > 1 and self.n_chunks > 1:
+            
+           if self.n_chunks == 1:
+              datasets = [np.asarray(data)]
+              return datasets
+
+           num_points = data.shape[0]
+           pp_chunks = num_points // n_chunks
+           datasets = []
+           for c in range(n_chunks-1):
+               datasets.append(np.asarray(data[pp_chunks*c:pp_chunks*(c+1)]))
+           datasets.append(np.asarray(data[(pp_chunks*(n_chunks-1)):num_points]))
 
             knn_data = Parallel(n_jobs=self.n_jobs)(
                 delayed(_core_dist_query,
